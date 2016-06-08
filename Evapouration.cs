702f22c37cb010669592aac1d8392b98393928d7
lisajguo <lisajguo@users.noreply.github.com>
2016-06-08 10:09:04 -0400

    Add files via upload

---
+++ Evapouration.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Evapouration : MonoBehaviour {
+
+	// Use this for initialization
+	//void Start () {
+	
+	//}
+	
+	// Update is called once per frame
+	//void Update () {
+
+    public bool isClick() { 
+        if (Input.GetMouseButtonDown(0))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+}
---
+++ Flotation.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+    public float yMin, yMax;
+
+}
+
+public class Flotation : MonoBehaviour {
+
+    public float speed;
+    public Boundary boundary;
+
+    public Evapouration isEvapourating { get; private set; }
+
+    void Update ()
+    {
+        isEvapourating = new Evapouration();
+        float moveVertical = Input.GetAxis("Vertical");
+
+        if (isEvapourating.isClick())
+        {
+            Vector3 movement = new Vector3(transform.position.x, moveVertical, 0.0f);
+            GetComponent<Rigidbody>().velocity = movement * speed;
+        }
+
+        GetComponent<Rigidbody>().position = new Vector3
+        (
+            transform.position.x, transform.position.y,
+            //Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.yMin, boundary.yMax), 
+            0.0f
+        );
+	}
+}
